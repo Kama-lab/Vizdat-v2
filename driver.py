60a9c6db8304c4f6b55cb5e782eba6cadecd9a4e
Kamron <soliev.kamronbek@gmail.com>
2021-11-16 17:48:37 +0200

    access

--- driver.py
+++ driver.py
@@ -1,13 +1,13 @@
 import os
 
-f = open('ignored_packs.txt','w')
+
 
 with open('requirements.txt') as f:
     line = f.readlines()
 
 for l in line:
+    f = open('ignored_packs.txt','w')
     print(f"{l.strip()}")
     strg = l + os.popen(f'sh run.sh {l.strip()}').read() + "\n" + "======================================================="
     f.write(strg)
-
-f.close()
+    f.close()
